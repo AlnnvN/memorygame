bbdf02fa8a4dfda0014e61bf1b6e058d8dbdaa52
Alan <alanvan3791@gmail.com>
2022-06-29 02:43:22 -0300

    core game finished

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const gameContainer = document.getElementById("game-container");
 const images = document.getElementById("images").children;
 
+var score = 0;
+
 LoadSquares();
 LinkImages();
-
+GameLogic();
 
 
 //FUNCTIONS
 
 function LoadSquares (){
+   
+
     for (let i = 0; i < 12; i++) {
         let sqrDiv = document.createElement("div");//create sqr div
         sqrDiv.classList.add("square-div");
@@ -17,6 +21,73 @@ function LoadSquares (){
         let sqr = document.createElement("div");//create sqr
         sqr.classList.add("square");
         sqrDiv.appendChild(sqr);
+    }
+
+    
+}
+
+function GameLogic(){
+    let choices = [];
+    let readyToAdd = true;
+    for (let i = 0; i < getSquareArray().length; i++) {
+        
+        getSquare(i).addEventListener('click',() => addChoice(getSquare(i)))
+
+        //sqr = getSquare(i);
+        //getSquare(i).onclick = addChoice;
+    }
+
+    function addChoice(sqr)
+    {
+        let img = sqr.firstElementChild;
+
+        if(choices[0] === undefined)
+        {
+            if(readyToAdd && img.className !== "image guessed"){
+                add();
+            }
+        }
+        else
+        {
+            if(readyToAdd && choices[0].parentElement != sqr && img.className !== "image guessed"){
+                add();
+            }
+        }
+        
+
+        function add() {
+            
+            console.log("showing image");
+            img.style.display = "flex";
+            choices.push(img);
+            checkChoice();
+        }
+    }
+    
+    function checkChoice()
+    {
+        
+        if(choices.length == 2)
+        {
+            readyToAdd = false;
+            if(choices[0].getAttribute("src") === choices[1].getAttribute("src"))
+            {
+                choices[0].classList.add("guessed");
+                choices[1].classList.add("guessed");
+                score++;
+                choices = [];
+                readyToAdd = true;
+            }
+            else{
+                setTimeout(hideImages,750);
+            }
+            function hideImages(){
+                choices[0].style.display = "none";
+                choices[1].style.display = "none";
+                choices = [];
+                readyToAdd = true;
+            }
+        }
     }
 }
 
@@ -29,7 +100,6 @@ function LinkImages(){
             let random = getRandomInt(0,sqr.length);
             sqr[random].appendChild(imgarr[b]);
             sqr.splice(random,1);
-            console.log(sqr.length);
         }
     }
     
@@ -39,7 +109,7 @@ function LinkImages(){
 //aux functions
 
 function getSquare(index){
-    return gameContainer.children[index].children;
+    return gameContainer.children[index].firstElementChild;
 }
 
 function getSquareArray(){
